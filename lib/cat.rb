0aedbfbab0fd847bceea6c2fae0d5691e1c69afb
John Mitropoulos <jmitropoulos@alumni.berklee.edu>
2020-10-08 07:39:44 +0000

    Automatically backed up by Learn

--- lib/cat.rb
+++ lib/cat.rb
@@ -1,3 +1,18 @@
 class Cat
-  # code goes here
+  
+  attr_accessor :name, :mood
+  attr_reader :owner
+  
+  @@all = []
+  
+  def initialize(name, owner, mood = "nervous")
+    @name = name
+    @owner = owner
+    @mood = mood
+  end
+  
+  def self.all
+    @@all
+  end
+  
 end
